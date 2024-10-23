e7122b3b724b63e004bf74489a7c783e4b36e2eb
Йоанна <morlineday@ukr.net>
2024-10-23 11:22:01 +0300

    fixed tasks distribution;

--- src/main/java/leonbet/com/parser/BookmakerParserApp.java
+++ src/main/java/leonbet/com/parser/BookmakerParserApp.java
@@ -33,37 +33,50 @@ public class BookmakerParserApp {
 	public static void main(String[] args) throws InterruptedException, IOException, ExecutionException {
 		ExecutorService executorService = Executors.newFixedThreadPool(MAX_COUNT_THREADS);
 		List<Callable<Sport>> tasks = new ArrayList<>();
-		CompletableFuture<String> mainResponse = HttpUtil.getRequestAndResponse(HttpUtil.getMainUrl());
 		
-		JSONArray sportMainList = new JSONArray(mainResponse.get());
-		for (int i = 0; i < sportMainList.length(); i++) {
-			JSONObject sportJson = sportMainList.getJSONObject(i);
-			String sportName = sportJson.getString(NAME);
-			if (SPORTS.contains(sportName)) {
-				Sport sport = new Sport(sportName, new ArrayList<>());
-				
-				List<League> leaguesList = new ArrayList<>();
-				JSONArray leagues = sportJson.getJSONArray(REGIONS);
-				for (int j = 0; j < leagues.length(); j++) {
-					JSONArray leagueArray = leagues.getJSONObject(j).getJSONArray(LEAGUES);
-					for (int k = 0; k < leagueArray.length(); k++) {
-						JSONObject leagueJson = leagueArray.getJSONObject(k);
-						Integer topOrder = leagueJson.getInt(TOP_ORDER);
-						Boolean isTop = leagueJson.getBoolean(TOP);
-						if (isTop) {
-							Long id = leagueJson.getLong(ID);
-							String name = leagueJson.getString(NAME);
-							String nameDefault = leagues.getJSONObject(j).getString(NAME_DEFAULT);
-							League league = new League(id, name, nameDefault, topOrder, new ArrayList<>());
-							leaguesList.add(league);
-						}
-					}					
-				}
-				Collections.sort(leaguesList);
-				sport.leagues().addAll(leaguesList);
-				AsyncTaskParse asyncTaskParse = new AsyncTaskParse(sport);
-				tasks.add(asyncTaskParse);
+		List<Sport> sports = new ArrayList<>();
+		executorService.submit(() -> {
+			CompletableFuture<String> mainResponse = HttpUtil.getRequestAndResponse(HttpUtil.getMainUrl());
+			String basicResponse = null;
+			try {
+				basicResponse = mainResponse.get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
 			}
+			
+			JSONArray sportMainList = new JSONArray(basicResponse);
+			for (int i = 0; i < sportMainList.length(); i++) {
+				JSONObject sportJson = sportMainList.getJSONObject(i);
+				String sportName = sportJson.getString(NAME);
+				if (SPORTS.contains(sportName)) {
+					Sport sport = new Sport(sportName, new ArrayList<>());
+					sports.add(sport);
+					List<League> leaguesList = new ArrayList<>();
+					JSONArray leagues = sportJson.getJSONArray(REGIONS);
+					for (int j = 0; j < leagues.length(); j++) {
+						JSONArray leagueArray = leagues.getJSONObject(j).getJSONArray(LEAGUES);
+						for (int k = 0; k < leagueArray.length(); k++) {
+							JSONObject leagueJson = leagueArray.getJSONObject(k);
+							Integer topOrder = leagueJson.getInt(TOP_ORDER);
+							Boolean isTop = leagueJson.getBoolean(TOP);
+							if (isTop) {
+								Long id = leagueJson.getLong(ID);
+								String name = leagueJson.getString(NAME);
+								String nameDefault = leagues.getJSONObject(j).getString(NAME_DEFAULT);
+								League league = new League(id, name, nameDefault, topOrder, new ArrayList<>());
+								leaguesList.add(league);
+							}
+						}					
+					}
+					Collections.sort(leaguesList);
+					sport.leagues().addAll(leaguesList);
+				}
+			}
+		}).get();
+		
+		for(Sport sport: sports) {
+			AsyncTaskParse asyncTaskParse = new AsyncTaskParse(sport);
+			tasks.add(asyncTaskParse);
 		}
 
 		List<Future<Sport>> futureData = executorService.invokeAll(tasks);
